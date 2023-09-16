717058f492ed808d9efbe85205c41d2b674c28d6
toofaniCoder <smartcoder500@gmail.com>
2023-09-16 16:22:30 +0530

    basic form added

--- src/routes/registration.jsx
+++ src/routes/registration.jsx
@@ -1,10 +1,26 @@
-import { Card, Stack, Typography } from "@mui/joy";
+import { Card, Stack, ToggleButtonGroup, Button } from "@mui/joy";
+import { useState } from "react";
 
 const Registration = () => {
+  const [value, setValue] = useState("student register");
   return (
-    <Stack sx={{py:10}}>
-      <Card sx={{width:0.4, mx:"auto"}}>
-        <Typography level="body-md" textAlign={"center"}>Registration Page</Typography>
+    <Stack sx={{ py: 10 }}>
+      <Card
+        sx={{
+          width: 0.3,
+          mx: "auto",
+          alignItems: "center",
+        }}
+      >
+        <ToggleButtonGroup
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        >
+          <Button value={"student register"}>Student Register</Button>
+          <Button value={"teacher register"}>Teacher Register</Button>
+        </ToggleButtonGroup>
       </Card>
     </Stack>
   );
